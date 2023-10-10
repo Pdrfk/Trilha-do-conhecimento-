4504af588b2548ffc3eff3b1e63e4b17998bb8e5
Pdrfk <138123055+Pdrfk@users.noreply.github.com>
2023-10-10 16:56:43 -0300

    Add files via upload

---
+++ funcao.py
@@ -0,0 +1,14 @@
+def somar(x, y):
+    return (x+y)
+
+def subtrair(x, y):
+   return (x-y)
+
+def dividir(x, y):
+    if(y==0):
+        return 0
+    else: 
+        return (x/y)
+
+def multiplicar(x, y):
+   return (x*y)
---
+++ main.py
@@ -0,0 +1,10 @@
+import funcao
+
+x = int(input("Digite a variavel x"))
+y = int(input("Digite a variavel y"))
+
+
+print("soma",funcao.somar(x,y))
+print("subtração",funcao.subtrair(x,y))
+print("divisão",funcao.dividir(x,y))
+print("multiplicação",funcao.multiplicar(x,y))
