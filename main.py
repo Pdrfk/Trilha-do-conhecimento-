27c14fc729c2a5a2b7f4c7a077554fdde73a7309
Pdrfk <138123055+Pdrfk@users.noreply.github.com>
2024-01-09 15:40:10 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,63 @@
-import funcao
+import speech_recognition as sr
+import pyttsx3 
+import datetime
+import wikipedia
+import pywhatkit
 
-x = int(input("Digite a variavel x"))
-y = int(input("Digite a variavel y"))
+audio = sr.Recognizer()
+maquina = pyttsx3.init()
 
+def executa_comando():
+    try:
+        with sr.Microphone() as source:
+            print ('ouvindo..')
+            voz = audio.listen(source)
+            comando = audio.recognize_google( voz, language='pt-BR')
+            comando = comando.lower()
+            if 'moana' in comando:
+                comando = comando.replace('moana', '')
+                maquina.say(comando)
+                maquina.runAndWait()
 
-print("soma",funcao.somar(x,y))
-print("subtração",funcao.subtrair(x,y))
-print("divisão",funcao.dividir(x,y))
-print("multiplicação",funcao.multiplicar(x,y))
+    except:
+        print('Microfone não está ok')
+
+    return comando
+    
+def comando_voz_usuario():
+        comando = executa_comando()
+        if 'horas' in comando:
+            hora = datetime.datetime.now().strftime('%H:%M')
+            maquina.say('Agora são' + hora)
+            maquina.runAndWait()
+        elif 'o que é' in comando:
+             procurar = comando.replace('o que é', '')
+             wikipedia.set_lang('pt')
+             resultado = wikipedia.summary(procurar,3)
+             print(resultado)
+             maquina.say(resultado)
+             maquina.runAndWait()
+
+        elif 'quem é' in comando:
+             procurar = comando.replace('quem é', '')
+             wikipedia.set_lang('pt')
+             resultado = wikipedia.summary(procurar,3)
+             print(resultado)
+             maquina.say(resultado)
+             maquina.runAndWait()
+
+        elif 'quem foi' in comando:
+             procurar = comando.replace('quem foi', '')
+             wikipedia.set_lang('pt')
+             resultado = wikipedia.summary(procurar,3)
+             print(resultado)
+             maquina.say(resultado)
+             maquina.runAndWait()
+        elif 'toque no youtube' in comando:
+             musica = comando.replace('toque', '')
+             resultado = pywhatkit.playonyt(musica)
+             maquina.say('abrindo a' + musica + 'no youtube')
+             maquina.runAndWait()
+            
+comando_voz_usuario()
+
